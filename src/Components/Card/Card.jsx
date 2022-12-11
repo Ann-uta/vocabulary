efc5fb6077bf79af916dbe4b1cca782670800419
Anna <momannuta@gmail.com>
2022-12-11 19:17:25 +0300

    learned

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,30 +1,36 @@
 import '../../Styles/Card.css';
 import { useState, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 //import wordsData from '../../wordsData.json';
 
 export default function Card (props) {
-   const [isCheck, setIsCheck] = useState(false);
-   const buttonRef = useRef();
+    const {id, russian, english, transcription, addLearned} = props;
+    const [isCheck, setIsCheck] = useState(false);
+    const buttonRef = useRef(null);
+    let location = useLocation();
 
-   function onClick() {
-    setIsCheck(!isCheck)
-   }   
-   useEffect(() => {
+    function onCheck() {
+        setIsCheck(!isCheck);
+        if (location.pathname === '/game')
+        addLearned();
+    }   
+    useEffect(() => {
     setIsCheck(false);
-}, [props.english])
+}, [english])
 
 useEffect(() => {
-    buttonRef.current.focus();
-}, [props.id, isCheck])
+    if (location.pathname === '/game') {
+    buttonRef.current.focus()}
+}, [id, isCheck, location.pathname])
 
     return (
         <div className='card__item' >
-            <p className='card__english'>{props.english}</p>
-            <p className= 'card__transcription'>{props.transcription}</p>
+            <p className='card__english'>{english}</p>
+            <p className= 'card__transcription'>{transcription}</p>
             <div className='card__translate' >               
                 {isCheck
-                    ? <div ref={buttonRef} className='card__russian'onClick={onClick}>{props.russian}</div>
-                    : <button ref={buttonRef} className='card__btn' onClick={onClick}>Проверить</button>
+                    ? <div ref={buttonRef} className='card__russian'onClick={onCheck}>{russian}</div>
+                    : <button ref={buttonRef} className='card__btn' onClick={onCheck}>Проверить</button>
                 }
             </div>                 
         </div>        
--- src/Components/CardGallery/CardGallery.jsx
+++ src/Components/CardGallery/CardGallery.jsx
@@ -6,7 +6,7 @@ import arrow from './arrow.png';
 export default function CardGallery(props){
 let {index = 0,
    // total
-} = props;
+  } = props;
 
 const [currentIndex, setIndex] = useState(index);
 
@@ -18,13 +18,21 @@ function onNextClick() {
 }
 let count = currentIndex+1;
 
+const id = wordsData[currentIndex].id
+const [learned, setLearned] = useState([]);
+
+function addLearned(){
+  if (!learned.includes(id)){
+  setLearned([...learned, id]);
+}}
+
     return (
       <div className='gallery_wrap'>
         <h1>Game</h1>
         <div className='gallery'>
             <div className="slider"><div className='slider__button'>
                 {count !== 1 &&  <img  onClick={onPrevClick} src={arrow} alt="left" />}</div>
-                <Card {...wordsData[currentIndex]}
+                <Card {...wordsData[currentIndex]} addLearned={addLearned}
                   /*english={wordsData[currentIndex].english}
                   transcription={wordsData[currentIndex].transcription}
                   russian={wordsData[currentIndex].russian}*/
@@ -34,6 +42,7 @@ let count = currentIndex+1;
                 {count !== wordsData.length && <img onClick={onNextClick} src={arrow} alt="right" />} </div>
             </div>
             <div className='slider__count'>{count}/{wordsData.length}</div>
+            <span>Изучено слов: {learned.length}</span>
         </div></div>
         )        
     }
